5ba0b2ce2a05be288fb669c93cd0dfad0ab9e558
marcozuccaroli <marco.zuccaroli@innoteam.it>
2018-07-06 12:47:39 +0200

    refactor, move handlig tab methods to test utils

--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -13,25 +13,4 @@ export class AppPage {
         return element(by.css('a.newtab'));
     }
 
-    closeOpenedTabs() {
-        browser.getAllWindowHandles().then(function (handles) {
-            for (let i = 1; i < handles.length; i++) {
-                if (handles[i]) {
-                    console.log('** testutils - closing tab: ' + i + ' **');
-                    browser.driver.switchTo().window(handles[i]);
-                    browser.driver.close();
-                }
-            }
-            browser.driver.switchTo().window(handles[0]);
-        });
-    }
-
-    getOpenedTabs() {
-        return browser.getAllWindowHandles();
-    }
-
-    goToTab(tab) {
-        return browser.switchTo().window(tab);
-    }
-
 }
--- e2e/multipleTabs.e2e-spec.ts
+++ e2e/multipleTabs.e2e-spec.ts
@@ -1,24 +1,27 @@
+import {TestUtils} from './testUtils';
 import {AppPage} from './app.po';
+
+const testUtils = new TestUtils();
 
 describe('angular-cli-tests-example App', () => {
     let page: AppPage;
 
     beforeAll(() => {
         page = new AppPage();
-        page.closeOpenedTabs();
+        testUtils.closeOpenedTabs();
     });
 
     afterEach(() => {
-        page.closeOpenedTabs();
+        testUtils.closeOpenedTabs();
     });
 
     it('should open a new tab with app homepage after clicking on "newtab" link', () => {
         page.navigateTo();
         page.getNewTabLink().click();
 
-        page.getOpenedTabs().then(function (tabs) {
+        testUtils.getOpenedTabs().then(function (tabs) {
             expect(tabs.length).toEqual(2);
-            page.goToTab(tabs[1]).then(function () {
+            testUtils.goToTab(tabs[1]).then(function () {
                 expect(page.getParagraphText()).toEqual('Welcome to app!');
             });
         });
